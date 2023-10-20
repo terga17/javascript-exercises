027e605bc49eb93988a407c21b9ab29262558caf
terga17 <terglav10@gmail.com>
2023-10-20 16:38:01 +0200

    Finished exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const lowerStr = string.toLowerCase().split("");
+    var reversedStr = lowerStr.reverse().join("");
+    return string === reversedStr;
+    
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(value) {
+num = Number(value);
+if(num < 0) return "OOPS";
+if(num === 0) return 0;
+
+let firstPrev = 1;
+let secondPrev = 0;
+
+for(let i = 2; i <= num; i++){
+    let current = secondPrev + firstPrev;
+    secondPrev = firstPrev;
+    firstPrev = current;
+}
+return firstPrev;
 
 };
 
